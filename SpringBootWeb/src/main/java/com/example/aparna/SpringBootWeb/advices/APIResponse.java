782587a3f7e039f51cc54d5fa29a57287cdd3fca
Aparna <aparnabanpela@gmail.com>
2026-02-07 18:20:03 +0530

    validation eception handler

---
+++ SpringBootWeb/src/main/java/com/example/aparna/SpringBootWeb/advices/APIResponse.java
@@ -0,0 +1,30 @@
+package com.example.aparna.SpringBootWeb.advices;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class APIResponse<T> {
+
+    @JsonFormat(pattern = "hh:mm:ss dd-MM-yyyy")
+    private LocalDateTime timeStamp;
+    private T data;
+    private APIError error;
+
+    public APIResponse() {
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public APIResponse(T data) {
+        this();
+        this.data = data;
+    }
+
+    public APIResponse(APIError error) {
+        this();
+        this.error = error;
+    }
+}
--- SpringBootWeb/src/main/java/com/example/aparna/SpringBootWeb/advices/GlobalExceptionHandler.java
+++ SpringBootWeb/src/main/java/com/example/aparna/SpringBootWeb/advices/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
 
 import java.util.NoSuchElementException;
 
@@ -12,11 +14,39 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<APIError> handleResourceNotFound(ResourceNotFoundException exception){
+    public ResponseEntity<APIResponse<?>> handleResourceNotFound(ResourceNotFoundException exception){
         APIError apiError = APIError.builder().
                 status(HttpStatus.NOT_FOUND).
                 message(exception.getMessage()).
                 build();
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<APIResponse<?>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        StringBuilder errorMessage = new StringBuilder();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errorMessage.append(error.getField())
+                                .append(": ")
+                                .append(error.getDefaultMessage())
+                                .append("; ")
+                );
+
+        APIError apiError = APIError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(errorMessage.toString())
+                .build();
+
+        return buildErrorResponseEntity(apiError);
+    }
+
+
+    private ResponseEntity<APIResponse<?>> buildErrorResponseEntity(APIError apiError) {
+        return new ResponseEntity<>(new APIResponse<>(apiError), apiError.getStatus());
     }
 }
